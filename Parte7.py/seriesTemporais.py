93b7f70d4f5a0c2d2935115079927477a9af614f
Felipe Tagawa <felipereisbkp@gmail.com>
2025-11-15 21:41:17 -0300

    feat: Séries Temporais

---
+++ Parte7.py/seriesTemporais.py
@@ -0,0 +1,16 @@
+# Análise de Tendências, Sazonalidades e Ciclos - Comportamento ao longo do tempo
+
+# Tendência: Crescente, Decrescente ou Estacionária(Desvio Padrão Menor)
+# Sazonalidade: Padrão de tempos em tempos
+# Ciclo: Representa flutuações de longo prazo que não segue um padrão fixo como a sazonalidade
+# Ruído: Componente imprevisível (resíduo ou aleatoriedade)
+
+# Decomposição: Extração dos componentes acima
+
+#   Tipos : Aditiva(Padrões próximos - picos de mesmo tamanho) e Multiplicativa (Picos muito diferentes)
+
+# Algoritmos de Previsão de Séries Temporais
+
+# 1. Holt-Winters - Melhor para previsões rápidas e simples, com padrões mais regulares e sazonalidade clara
+# 2. ARIMA - Mais robusto para séries complexas (SARIMA(sazonalidade), ARIMAX(variáveis externas) e SARIMAX)
+
